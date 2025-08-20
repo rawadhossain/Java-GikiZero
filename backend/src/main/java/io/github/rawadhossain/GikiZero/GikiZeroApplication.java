296c64352d05783d8dba44a278595e3a633a3ad6
Rawad Hossain <rawad.hossain00@gmail.com>
2025-08-21 01:49:10 +0600

    initial springboot backend setup

---
+++ backend/src/main/java/io/github/rawadhossain/GikiZero/GikiZeroApplication.java
@@ -0,0 +1,13 @@
+package io.github.rawadhossain.GikiZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GikiZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GikiZeroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/rawadhossain/GikiZero/GikiZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rawadhossain.GikiZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GikiZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
